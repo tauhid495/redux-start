6bf55c840e4bef0d9f039c4093f877325763e1a1
tauhid495 <tauhid495@gmail.com>
2022-12-04 03:01:04 +0600

    ki

---
+++ src/pages/Home.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import ProductCard from "../components/ProductCard";
+
+const Home = () => {
+
+const [products, setProducts]=useState([]);
+
+useEffect(()=>{
+  fetch('http://localhost:5000/products').then(res=>res.json())
+  .then(data=>setProducts(data))
+},[])
+
+const state = useSelector((state)=> state);
+console.log(state);
+
+  return (
+    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10'>
+      {
+        products.map((product, index)=> <ProductCard key={index} product={product}/>)
+      }
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/redux/reducer/productReducer.js
@@ -0,0 +1,26 @@
+import { ADD_TO_CART, REMOVE_FROM_CART } from "../actionTypes/actionTytpes";
+
+const initialState = {
+    cart: [],
+};
+
+const productReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case ADD_TO_CART:
+            return {
+                ...state,
+                cart:[...state.cart, action.payload],
+            };
+
+        case REMOVE_FROM_CART:
+            return {
+                ...state,
+                cart: state.cart.filter((product)=>product._id!==action.payload._id)
+            };
+
+        default:
+            return state;
+    }
+};
+
+export default productReducer;
